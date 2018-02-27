f72d82fb08da4bcf9ad2ebfae8d8a420d7c35d62
Your Name <you@example.com>
2018-02-27 19:34:39 +0800

    mergesam

--- go_read_bam.go
+++ go_read_bam.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/biogo/hts/bam"
-	"github.com/biogo/hts/sam"
+	//	"github.com/biogo/hts/sam"
 	"io"
 	"log"
 	"os"
 )
 
+func pairs(stat map[string]int32) error {
+	v, _ := stat["Total_pairs_processed"]
+	stat["Total_pairs_processed"] = v + 1
+	//fmt.Println(stat["Total_pairs_processed"])
+	return nil
+}
+
 func main() {
-	bam_read, _ := os.Open("D:\\seqyuan\\go_bowtiePairing\\SC_Pur_01_Lib1_lane1_mm9.bwt2pairs.bam")
+	bam_read, _ := os.Open("D:\\seqyuanGithub\\goHiC\\goHiC\\SC_Pur_01_Lib1_lane1_R1_mm9.bwt2merged.bam")
 	defer bam_read.Close()
 	br, err := bam.NewReader(bam_read, 1)
 	if err != nil {
@@ -18,7 +25,7 @@ func main() {
 	}
 	defer br.Close()
 
-	f_out, _ := os.OpenFile("D:\\seqyuan\\go_bowtiePairing\\out.bam", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+	f_out, _ := os.OpenFile("D:\\seqyuanGithub\\goHiC\\goHiC\\out.bam", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
 	defer f_out.Close()
 	bw, err := bam.NewWriter(f_out, br.Header(), 1)
 	if err != nil {
@@ -26,8 +33,22 @@ func main() {
 	}
 	defer bw.Close()
 
+	var stat = map[string]int32{
+		"Total_pairs_processed":     0,
+		"Unmapped_pairs":            0,
+		"Pairs_with_Singleton":      0,
+		"Low_qual_pairs":            0,
+		"Unique_paired_alignments":  0,
+		"Multiple_pairs_alignments": 0,
+	}
+
+	_ = pairs(stat)
+
 	aa := 0
+	m2 := make(map[string]int)
+
 	for {
+		_ = pairs(stat)
 		r, err := br.Read()
 		if err == io.EOF {
 			break
@@ -35,18 +56,57 @@ func main() {
 		if err != nil {
 			log.Fatalf("failed to read BAM record: %v", err)
 		}
-
-		_, ok := r.Tag([]byte("ASsss"))
-
-		fmt.Println(r.Name, ok)
-		if sam.IsValidRecord(r) == false {
-			fmt.Println("000000000000000")
-		}
+		/*
+			if sam.IsValidRecord(r) == false {
+				fmt.Println("000000000000000")
+			}
+		*/
 
 		err = bw.Write(r)
+
+		aux, ok := r.Tag([]byte("AS"))
+		if aux != nil && ok {
+			primary := aux.Value()
+
+			switch b := primary.(type) {
+			case int:
+				//fmt.Println("int", b)
+				m2["int"] = 0
+			case string:
+				//fmt.Println("string", b)
+				m2["string"] = 0
+			case int8:
+				//fmt.Println("int8", b)
+				m2["int8"] = 0
+			case uint8:
+				//fmt.Println("uint8", b)
+				m2["uint8"] = 0
+			case int32:
+				//fmt.Println("int32", b)
+				m2["int32"] = 0
+			default:
+				//fmt.Println("else", b)
+				m2["else"] = 0
+				fmt.Println(b)
+			}
+
+		}
+
 		aa += 1
-		if aa > 5 {
+		if aa > 10 {
 			break
 		}
+	}
+	fmt.Println(aa)
+	fmt.Println(m2)
+	fmt.Println(stat)
+
+	qwe := int8(100)
+	erio := uint8(99)
+
+	if int8(erio) > qwe {
+		fmt.Println(">")
+	} else {
+		fmt.Println("<")
 	}
 }
--- mergeSAM.go
+++ mergeSAM.go
@@ -7,30 +7,83 @@ import (
 	"io"
 	"log"
 	"os"
+	//"strconv"
 )
 
-func pairs(r1 *sam.Record, r2 *sam.Record, stat *map[string]int, mapq int, bw *Writer) {
-	stat["Total_pairs_processed"] += 1
+//func pairs(r1 *sam.Record, r2 *sam.Record, stat *map[string]int, mapq int, bw *Writer) {
+
+func pairs(r1 *sam.Record, r2 *sam.Record, stat map[string]int, mapq int) error {
+	v, _ := stat["Total_pairs_processed"]
+	stat["Total_pairs_processed"] = v + 1
 
 	if sam.IsValidRecord(r1) == false && sam.IsValidRecord(r1) == false {
-		stat["Unmapped_pairs"] += 1
-		return
+		v, _ = stat["Unmapped_pairs"]
+		stat["Unmapped_pairs"] = v + 1
+		return nil
 	}
 
 	if sam.IsValidRecord(r1) == false || sam.IsValidRecord(r1) == false {
-		stat["Pairs_with_Singleton"] += 1
-		return
+		v, _ = stat["Pairs_with_Singleton"]
+		stat["Pairs_with_Singleton"] = v + 1
+		return nil
 	}
 
-	if r1.MapQ < mapq || r2.MapQ < mapq {
-		stat["Low_qual_pairs"] += 1
-		return
+	if int(r1.MapQ) < mapq || int(r2.MapQ) < mapq {
+		v, _ = stat["Low_qual_pairs"]
+		stat["Low_qual_pairs"] = v + 1
+		return nil
 	}
 
+	if is_unique_bowtie2(r1) && is_unique_bowtie2(r2) {
+		v, _ = stat["Unique_paired_alignments"]
+		stat["Unique_paired_alignments"] = v + 1
+		return nil
+	} else {
+		v, _ = stat["Multiple_pairs_alignments"]
+		stat["Multiple_pairs_alignments"] = v + 1
+		return nil
+	}
+
+	return nil
+}
+
+func is_unique_bowtie2(r *sam.Record) (ret bool) {
+	ret = false
+
+	var primary1 int8
+	var secondary1 int8
+
+	aux, ok := r.Tag([]byte("AS"))
+	if aux != nil && ok {
+		aux2, ok2 := r.Tag([]byte("XS"))
+		if aux2 != nil && ok2 {
+			primary := aux.Value()
+			secondary := aux2.Value()
+			switch primary.(type) {
+			case int8:
+				primary1 = primary.(int8)
+			case uint8:
+				primary1 = int8(primary.(uint8))
+			}
+
+			switch secondary.(type) {
+			case int8:
+				secondary1 = secondary.(int8)
+			case uint8:
+				secondary1 = int8(secondary.(uint8))
+			}
+
+			if primary1 > secondary1 {
+				ret = true
+			}
+			ret = true
+		}
+	}
+	return ret
 }
 
 func main() {
-	bam_read1, _ := os.Open("D:\\seqyuan\\go_bowtiePairing\\SC_Pur_01_Lib1_lane1_R1_mm9.bwt2merged.bam")
+	bam_read1, _ := os.Open("D:\\seqyuanGithub\\goHiC\\goHiC\\SC_Pur_01_Lib1_lane1_R1_mm9.bwt2merged.bam")
 	defer bam_read1.Close()
 	br1, err := bam.NewReader(bam_read1, 1)
 	if err != nil {
@@ -38,7 +91,7 @@ func main() {
 	}
 	defer br1.Close()
 
-	bam_read2, _ := os.Open("D:\\seqyuan\\go_bowtiePairing\\SC_Pur_01_Lib1_lane1_R2_mm9.bwt2merged.bam")
+	bam_read2, _ := os.Open("D:\\seqyuanGithub\\goHiC\\goHiC\\SC_Pur_01_Lib1_lane1_R2_mm9.bwt2merged.bam")
 	defer bam_read2.Close()
 	br2, err := bam.NewReader(bam_read2, 1)
 	if err != nil {
@@ -46,13 +99,15 @@ func main() {
 	}
 	defer br2.Close()
 
-	f_out, _ := os.OpenFile("D:\\seqyuan\\go_bowtiePairing\\out.bam", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
-	defer f_out.Close()
-	bw, err := bam.NewWriter(f_out, br.Header(), 1)
-	if err != nil {
-		log.Fatalf("failed to write BAM: %v", err)
-	}
-	defer bw.Close()
+	/*
+		f_out, _ := os.OpenFile("D:\\seqyuan\\go_bowtiePairing\\out.bam", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+		defer f_out.Close()
+		bw, err := bam.NewWriter(f_out, br.Header(), 1)
+		if err != nil {
+			log.Fatalf("failed to write BAM: %v", err)
+		}
+		defer bw.Close()
+	*/
 
 	var stat = map[string]int{
 		"Total_pairs_processed":     0,
@@ -64,6 +119,7 @@ func main() {
 	}
 
 	aa := 0
+
 	for {
 		r1, err := br1.Read()
 		if err == io.EOF {
@@ -85,11 +141,12 @@ func main() {
 			log.Fatalf("Forward and reverse reads not paired. Check that BAM files are sorted.")
 		}
 
-		fmt.Println(r.Name, r.Ref, r.Pos, r.Seq)
-		err = bw.Write(r)
+		pairs(r1, r2, stat, 0)
+		//err = bw.Write(r)
 		aa += 1
-		if aa > 5 {
+		if aa == 10000000 {
 			break
 		}
 	}
+	fmt.Println(stat)
 }
